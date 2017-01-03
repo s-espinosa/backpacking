5f14b60c866d59f9f9685397344e69f1737be27d
Sal Espinosa <sespinos@gmail.com>
2017-01-02 20:26:30 -0700

    Move spec back to include setup

--- spec/features/user_adds_pen_to_backpack_spec.rb
+++ spec/features/user_adds_pen_to_backpack_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "User adds a pen to their backpack", type: :feature do
-  scenario "a message is displayed" do
+  before(:all) do
     Item.create!(
       name: "Rollerball Pen",
       image_url: "/images/rollerball_pen.jpeg"
     )
+  end
 
+  scenario "a message is displayed" do
     visit root_path
     click_button "Add Item"
 
