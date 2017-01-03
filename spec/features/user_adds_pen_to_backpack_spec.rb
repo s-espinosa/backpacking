e8e835ee45c482664b79c2ab21a4d47bbd834199
Sal Espinosa <sespinos@gmail.com>
2017-01-02 20:08:26 -0700

    Update feature test

--- spec/features/user_adds_pen_to_backpack_spec.rb
+++ spec/features/user_adds_pen_to_backpack_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "User adds a pen to their backpack", type: :feature do
-  scenario "displays capture message and backpack content" do
+  before(:all) do
     Item.create!(
       name: "Rollerball Pen",
       image_url: "/images/rollerball_pen.jpeg"
     )
+  end
 
+  scenario "a message is displayed" do
     visit root_path
-
-    expect(page).to have_content("Backpack: 0")
-
     click_button "Add Item"
 
     expect(page).to have_content("You now have 1 Rollerball Pen.")
-    expect(page).to have_content("Backpack: 1")
   end
 end
 
