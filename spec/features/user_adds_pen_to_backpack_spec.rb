e5d14c9b6d939c934c76bac30baa74b7471499cf
Sal Espinosa <sespinos@gmail.com>
2017-01-02 20:22:54 -0700

    Update spec to work with DatabaseCleaner

--- spec/features/user_adds_pen_to_backpack_spec.rb
+++ spec/features/user_adds_pen_to_backpack_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "User adds a pen to their backpack", type: :feature do
-  before(:all) do
+  scenario "a message is displayed" do
     Item.create!(
       name: "Rollerball Pen",
       image_url: "/images/rollerball_pen.jpeg"
     )
-  end
 
-  scenario "a message is displayed" do
     visit root_path
     click_button "Add Item"
 
